e94beb3ffa5a85411ecbe8b717c315b283589eb5
unknown <Administrator@DBC-JN80673.lmetb.ie>
2022-12-16 09:50:38 +0000

    Camera

--- SuperCarApp/SuperCarApp/Profile.xaml.cs
+++ SuperCarApp/SuperCarApp/Profile.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -36,5 +36,37 @@ namespace SuperCarApp
         {
             await Navigation.PushAsync(new mappage());
         }
+
+
+        async void Button_Upload(System.Object sender, System.EventArgs e)
+        {
+            var result = await MediaPicker.PickPhotoAsync(new MediaPickerOptions
+            {
+                Title = "Please pick a photo"
+            });
+
+            if (result != null)
+            {
+                var stream = await result.OpenReadAsync();
+
+                resultImage.Source = ImageSource.FromStream(() => stream);
+            }
+        }
+
+        async void Button1_Take(System.Object sender, System.EventArgs e)
+        {
+            var result = await MediaPicker.CapturePhotoAsync();
+
+            if (result != null)
+            {
+                var stream = await result.OpenReadAsync();
+
+                resultImage.Source = ImageSource.FromStream(() => stream);
+            }
+        }
+
+
+
+
     }
 }
--- SuperCarApp/SuperCarApp/obj/Debug/netstandard2.0/Profile.xaml.g.cs
+++ SuperCarApp/SuperCarApp/obj/Debug/netstandard2.0/Profile.xaml.g.cs
@@ -17,8 +17,12 @@ namespace SuperCarApp {
     public partial class Profile : global::Xamarin.Forms.ContentPage {
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.Image resultImage;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
         private void InitializeComponent() {
             global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(Profile));
+            resultImage = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Image>(this, "resultImage");
         }
     }
 }
